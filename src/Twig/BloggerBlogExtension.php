fc4871d0bd064b9ecfc4888c5573e64793456b8d
kaci <bader.kaci@gmail.com>
2020-05-22 15:42:36 +0200

    add BloggerBlogExtension

---
+++ src/Twig/BloggerBlogExtension.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class BloggerBlogExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
+            new TwigFilter('created_ago', [$this, 'createdAgo']),
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('function_name', [$this, 'doSomething']),
+        ];
+    }
+
+   /* public function doSomething($value)
+    {
+        // ...
+    } */
+
+    public function createdAgo(\DateTime $dateTime)
+    {
+        $delta = time() - $dateTime->getTimestamp();
+        if ($delta < 0)
+            throw new \Exception("createdAgo is unable to handle dates in the future");
+
+        $duration = "";
+        if ($delta < 60)
+        {
+            // Seconds
+            $time = $delta;
+            $duration = $time . " second" . (($time > 1) ? "s" : "") . " ago";
+        }
+        else if ($delta <= 3600)
+        {
+            // Mins
+            $time = floor($delta / 60);
+            $duration = $time . " minute" . (($time > 1) ? "s" : "") . " ago";
+        }
+        else if ($delta <= 86400)
+        {
+            // Hours
+            $time = floor($delta / 3600);
+            $duration = $time . " hour" . (($time > 1) ? "s" : "") . " ago";
+        }
+        else
+        {
+            // Days
+            $time = floor($delta / 86400);
+            $duration = $time . " day" . (($time > 1) ? "s" : "") . " ago";
+        }
+
+        return $duration;
+    }
+}
