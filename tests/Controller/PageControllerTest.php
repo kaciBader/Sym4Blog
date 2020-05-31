ec5857f1767554b7f03300ca2ff043e01e2db472
kaci <bader.kaci@gmail.com>
2020-05-31 23:44:19 +0200

    update fonctional tests testIndex and testAbout function for PageControllerTest class

--- tests/Controller/PageControllerTest.php
+++ tests/Controller/PageControllerTest.php
@@ -7,16 +7,45 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class PageControllerTest extends WebTestCase
 {
+
+    public function testIndex()
+    {
+        $client = static::createClient(); // create client
+        $crawler = $client->request('GET', '/'); // send request
+        $this->assertResponseIsSuccessful(); // 1 test successful reponse
+        $this->assertResponseStatusCodeSame(200); // 2 
+        $this->assertEquals(200, $client->getResponse()->getStatusCode()); 
+        $this->assertSelectorExists('html'); // test that <html> exists
+        $this->assertPageTitleSame('sym4blog'); // test page title
+
+        // Vérifie qu'il y a des articles dans la page
+        $this->assertTrue($crawler->filter('article.blog')->count() > 0);
+        // check section Tag Cloud exists
+        $this->assertSelectorTextContains('.section header h3', 'Tag Cloud');
+        // test section Latest Comments exists
+        $this->assertEquals('Latest Comments',  $crawler->filter('.section header h3')->eq(1)->text());
+        
+
+    }
     public function testAbout()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/about');
 
-        $this->assertEquals(1, $crawler->filter('h1:contains("About sym4blog")')->count());
-    }
+        // test successful response 
+        $this->assertResponseIsSuccessful();
+        // test page title
+         $this->assertPageTitleSame('sym4blog-about'); 
 
+         // check section Tag Cloud exists
+        $this->assertSelectorTextContains('.section header h3', 'Tag Cloud');
+        // test section Latest Comments exists
+        $this->assertEquals('Latest Comments',  $crawler->filter('.section header h3')->eq(1)->text());
 
+    } 
+
+/*
     public function testIndex()
 	{
 	    $client = static::createClient();
@@ -34,6 +63,6 @@ class PageControllerTest extends WebTestCase
     	// Vérifie que h2 contient bien le titre de l'article
     	$this->assertEquals(1, $crawler->filter('h2:contains("' . $blogTitle .'")')->count());
 	}
-
+*/
 
 }
